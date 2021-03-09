7dca80f4e2f79c91678bd250c04164189afaa1ef
Saponja <jsaponjic512@gmail.com>
2021-03-09 22:40:29 +0100

    Izmenjena metoda pronadjiArtikal u IMagacin i implementirane metode u klasi Magacin

--- src/logika/Magacin.java
+++ src/logika/Magacin.java
@@ -1,21 +1,54 @@
 package logika;
 
+import java.util.LinkedList;
+
 import domen.Artikal;
 
 public class Magacin implements IMagacin {
+	
+	private LinkedList<Artikal> artikli = new LinkedList<Artikal>();
 
 	@Override
 	public void dodajArtikal(Artikal artikal) {
+		if(artikal == null)
+			throw new NullPointerException("Artikal ne sme da bude null");
+		for (Artikal a : artikli) {
+			if(a.getSifra() == artikal.getSifra()) {
+				a.setKolicina(a.getKolicina() + 1);
+				return;
+			}
+		}
 		
+		artikli.add(artikal);
 	}
 
 	@Override
 	public void izbaciArtikal(Artikal artikal) {
 		
+		if(artikal == null || !artikli.contains(artikal))
+			throw new RuntimeException("Lista trenutnih artikala ne sadrzi dati artikal");
+		
+		artikli.remove(artikal);
+		
 	}
 
 	@Override
-	public void pronadjiArtikal(int sifra) {
+	public Artikal pronadjiArtikal(int sifra) {
+		
+		Artikal rezultat = null;
+		
+		if(sifra <= 0)
+			throw new RuntimeException("Sifra proizvoda ne sme biti manja od 0");
+		
+		for (Artikal a : artikli) {
+			if(a.getSifra() == sifra)
+				rezultat = a;
+		}
+		
+		if(rezultat == null)
+			throw new RuntimeException("Artikal sa datom sifrom ne postoji u listi");
+		else
+			return rezultat;
 		
 	}
 
