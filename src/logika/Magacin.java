b5d809aef4d8b285bd8c04efb2e42f6a80a0359d
Saponja <jsaponjic512@gmail.com>
2021-03-09 22:26:42 +0100

    Dodata klasa Magacin koja implementira IMagacin, ali tako da su metode nefunkcionalne

---
+++ src/logika/Magacin.java
@@ -0,0 +1,22 @@
+package logika;
+
+import domen.Artikal;
+
+public class Magacin implements IMagacin {
+
+	@Override
+	public void dodajArtikal(Artikal artikal) {
+		
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal artikal) {
+		
+	}
+
+	@Override
+	public void pronadjiArtikal(int sifra) {
+		
+	}
+
+}
